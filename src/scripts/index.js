a1cc0bb129cd12343faf94aa84c3a6857b150218
René Mäkeler <maekeler@marmalade.de>
2018-05-21 15:13:58 +0200

    Add babel-polyfill to support async/await

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,7 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import App from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app-root')
-);
+ReactDOM.render(<App />, document.getElementById('app-root'))
